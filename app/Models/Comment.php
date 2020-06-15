076119981b6021a2b5ae72ac15e1076c070dada3
daisuke yamazaki <daisuke.yamazaki.1008@gmail.com>
2020-06-15 23:46:29 +0900

    initial

---
+++ app/Models/Comment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
+class Comment extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'text'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getComments(Int $tweet_id)
+    {
+        return $this->with('user')->where('tweet_id', $tweet_id)->get();
+    }
+}
